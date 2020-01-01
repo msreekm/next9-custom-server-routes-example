cbacfff3c2a132735c1fefe169b4fc01b43d5982
sreekumar <sreekumar@roofstock.com>
2019-12-31 18:56:09 -0800

    image

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import { withRouter } from "next/router";
 
 const About = ({ router }) => (
-  <h1>
-    About foo {router.query.foo} <br />
-    <a href="/public.txt">Public asset</a>
-    <br />
-    <a href="/static/images/fake-image.txt">test</a>
-  </h1>
+  <>
+    <h1>
+      About foo {router.query.foo} <br />
+      <a href="/public.txt">Public asset</a>
+      <br />
+    </h1>
+    <img href="/path.jpeg"></img>
+  </>
 );
 
 export default withRouter(About);
